9fb8c0fdf126b73d80f6dbada7c777c85f299965
Hrabr <emecandrei83@gmail.com>
2021-10-06 17:35:16 +0300

    aded finish HW from bot

---
+++ NameParserTest.java
@@ -0,0 +1,21 @@
+class NameParser {
+    public String parse(String[] names) {
+        StringBuilder a = new StringBuilder();
+        for (int i = 0; i < names.length; i++) {
+
+            a.append(names[i].split(" ")[0]);
+            if (i != names.length - 1) {
+                a.append(", ");
+            }
+        }
+        return a.toString();
+    }
+}
+ class NameParserTest {
+    public static void main(String[] args) {
+        String[] names = {"John Doe", "Bill Mask", "Nigua Joshua"};
+
+        //John, Bill, Nigua
+        System.out.println(new NameParser().parse(names));
+    }
+}
